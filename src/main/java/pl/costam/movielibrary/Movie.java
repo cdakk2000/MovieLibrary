0e9c5c010c09b0fd499d018f5080967aafe8a6a5
Jakub Kostrzewa <cdakk2000@gmail.com>
2022-12-13 16:31:27 +0100

    CRUD

---
+++ src/main/java/pl/costam/movielibrary/Movie.java
@@ -0,0 +1,14 @@
+package pl.costam.movielibrary;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+    private int id;
+    private String name;
+    private int rating;
+}
---
+++ src/test/java/pl/costam/movielibrary/MovielibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.costam.movielibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovielibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
